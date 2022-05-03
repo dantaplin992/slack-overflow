feb709939068582ee6f54d6f982d452e98ebb463
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-05-03 17:27:35 +0100

    io refactored into its own file

---
+++ api/public/scripts/socketConnection.js
@@ -0,0 +1,24 @@
+
+class socketConnection {
+  constructor(io) {
+    this.io = io
+  }
+
+
+
+}
+
+function chat(io) {
+  io.on('connection', (socket) => {
+  console.log("socket connected")
+  socket.emit('handshake', 'hello client')
+
+  socket.on('buttonPress', (msg) => {
+    console.log(msg)
+
+    io.emit('displayMessage', msg)
+  })
+})
+};
+
+module.exports = chat
