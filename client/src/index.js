1f8ce1c6a5f00b87de6bea15ddc4910ca5e9d9d0
Jake Dance <jake.dance1991@gmail.com>
2022-05-04 15:39:08 +0100

    added tailwind css environment for react

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,11 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(<h1 className="text-3xl italic">
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </h1>
 );
 
 // If you want to start measuring performance in your app, pass a function
