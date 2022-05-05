6e816542d95a1ca585700bd326f22eec0ffa3f5f
Jake Dance <jake.dance1991@gmail.com>
2022-05-05 12:17:00 +0100

    created SideBar for Rooms

--- client/src/components/chat/Rooms.js
+++ client/src/components/chat/Rooms.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import SideBar from './SideBar/SideBar';
 
 class Rooms extends React.Component {
   render = () => {
     return (
-      <div className='Rooms'>
-        Rooms
+      <div className="flex">
+        <SideBar />
       </div>
     )
   }
