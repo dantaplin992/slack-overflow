7d476f9878d312f705364ecc2693ee4b826990bc
Jake Dance <jake.dance1991@gmail.com>
2022-05-10 14:59:56 +0100

    amended styling

--- client/src/components/userAuth/Login.js
+++ client/src/components/userAuth/Login.js
@@ -19,10 +19,10 @@ class Login extends React.Component {
     }
       
       return (
-        <div className="flex flex-col justify-center items-center h-screen bg-slate-100">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-5 mb-4" onSubmit={handleSubmit}>
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-800">
+        <form className="bg-gray-700 shadow-md rounded px-8 pt-6 pb-5 mb-4" onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 text-left" htmlFor="email">
+            <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="email">
               Email
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
@@ -32,7 +32,7 @@ class Login extends React.Component {
             autoComplete='email' />
           </div>
          <div className="mb-6">
-           <label className="block text-gray-700 text-sm font-bold mb-2 text-left" htmlFor="password">
+           <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="password">
               Password
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
@@ -47,8 +47,8 @@ class Login extends React.Component {
               value="Login" />
            </div>
         <div className="mt-5">
-          <p className="text-center text-gray-500 text-xs">Don't have an account?</p>
-          <button className="text-center text-gray-500 hover:text-purple-600 text-xs"
+          <p className="text-center text-gray-400 text-xs">Don't have an account?</p>
+          <button className="text-center text-gray-400 hover:text-purple-600 text-xs"
                   onClick={signUp}>Sign Up</button>
         </div>
        </form>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,26 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      animation: {
+        tilt: 'tilt 10s infinite linear'
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(1deg)",
+          },
+          "75%": {
+            transform: "rotate(-1deg)",
+          }
+          },
+        },
+      },
+    },
+  
   plugins: [
   ],
 }
