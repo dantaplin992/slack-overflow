c080e70b568de8be00ea1f05299fa91ea198df0e
Jon Clarke <jonclarke1984@googlemail.com>
2022-05-10 17:27:28 +0100

    edit profile modal styling in place

--- client/src/components/chat/EditProfile.js
+++ client/src/components/chat/EditProfile.js
@@ -2,51 +2,124 @@ function EditProfile({ currentUser, toggleEditProfileModal }) {
   const { firstName, lastName, email, icon, displayName } = currentUser
 
   return(
-        <><div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Edit Profile
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={toggleEditProfileModal}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
+        <><div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto">
+                <div className="relative p-4 flex-auto">
                   <div className="my-4 text-slate-500 text-lg leading-relaxed">
-                    A form will be here
+                    <h2 className="text-center bg-gray-700 pt-3">Edit Profile</h2>
+                  <form className="bg-gray-700 shadow-md rounded px-8 pt-6 pb-5 mb-4" onSubmit={toggleEditProfileModal}>
+      <div className="flex -mx-3">
+       <div className="w-1/2 px-3 mb-5">
+       <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="firstName">
+          First Name
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" 
+          type='text'
+           name="firstName"
+           placeholder={`${firstName}`}
+           value=""
+           autoComplete='First Name' />
+        </div>
+      
+
+      <div class="w-1/2 px-3 mb-5">
+       <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="lastName">
+          Last Name
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          type='text'
+           name="lastName"
+           placeholder={`${lastName}`}
+           autoComplete='Last Name' />
+      </div>
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="email">
+          Email
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+        type='email'
+        name="email"
+        placeholder={`${email}`}
+        autoComplete='email' />
+      </div>
+
+     <div className="flex -mx-3">
+     <div class="w-1/2 px-3 mb-5">
+       <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="password">
+          Password
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          type='password'
+           name="password"
+           placeholder='Password'
+           autoComplete='password' />
+      </div>
+
+      <div class="w-1/2 px-3 mb-5">
+       <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="confirmPassword">
+          Confirm Password
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+          type='password'
+           name="confirmPassword"
+           placeholder='Confirm password'
+           autoComplete='password' />
+      </div>
+      </div>
+
+
+      <div className="mb-6">
+        <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="displayName">
+          Display Name
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type='text'
+          name="displayName"
+          placeholder={`${displayName}`}
+          autoComplete='Display Name' />
+      </div>
+
+      <div className="mb-6">
+        <label className="block text-gray-400 text-sm font-bold mb-2 text-left" htmlFor="icon">
+          Icon Url
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+          type='text'
+          name="icon"
+          placeholder={`${icon}`}
+          autoComplete='Icon Url' />
+      </div>
+
+
+       <div className="flex justify-center">
+           <ul>
+              <li>
+         <div className="relative group">
+           <div className="edgeGlow"></div>
+        <input className="relative bg-gray-600 group-hover:text-white text-gray-500 
+                          transition duration-1000 font-bold py-2 px-4 rounded-lg 
+                          focus:outline-none focus:shadow-outline cursor-pointer"
+          type="submit"
+          value="Submit" />
+          </div>
+              </li>
+              <li>
+          <button className="group-hover:text-white text-gray-500 mt-4"
+                  onClick={toggleEditProfileModal}>
+                  Cancel
+          </button>
+              </li>
+           </ul>
+       
+       </div>
+   </form>
                   </div>
                 </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={toggleEditProfileModal}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={toggleEditProfileModal}
-                  >
-                    Save Changes
-                  </button>
-                </div>
+                
               </div>
-            </div>
+           
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
           </>
