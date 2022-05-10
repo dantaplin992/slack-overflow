2461134fe22000a7c507805f126e9893a56546f4
Jake Dance <jake.dance1991@gmail.com>
2022-05-10 21:24:38 +0100

    added image file

--- client/src/components/chat/Banner.js
+++ client/src/components/chat/Banner.js
@@ -23,7 +23,7 @@ function Banner(props) {
     <div className='Banner'>
        <div>
            <button type="button" className='bannerProfile' onClick={toggleDropdown}>
-           <img className="w-6 h-6 mr-2 rounded-full shadow-lq" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+           <img className="w-6 h-6 mr-2 rounded-full shadow-lq" src={icon}/>
            {firstName} {lastName}
        </button>
        {showDropdown ? (
