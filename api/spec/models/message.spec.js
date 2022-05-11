3b35f26e460a17148e289b9d602ecd863440a984
shannon white <shannonw-l@hotmail.co.uk>
2022-05-11 17:13:37 +0100

    spec database tests update

--- api/spec/models/message.spec.js
+++ api/spec/models/message.spec.js
@@ -1,5 +1,5 @@
-var mongoose = require("mongoose");
-require("../mongodb_helper");
+var mongoose = require("mongoose")
+require("../mongodb_helper")
 var Message = require("../../models/message")
 
 const UserSchema = new mongoose.Schema({
@@ -8,9 +8,10 @@ const UserSchema = new mongoose.Schema({
   displayName: String,
   icon: String,
   password: String,
-});
+  email: String
+})
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema)
 
 const mockUser = new User({
   firstName: "John",
@@ -18,37 +19,38 @@ const mockUser = new User({
   displayName: "John Doe",
   icon: "https://avatars0.githubusercontent.com/u/174825?s=460&v=4",
   password: "password",
-});
+  email: "test@test.com"
+})
 
 
 describe("Message model", () => {
     beforeEach((done) => {
       mongoose.connection.collections.messages.drop(() => {
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it("has a message", () => {
-        var message = new Message({ message: "some message" });
-        expect(message.message).toEqual("some message");
-      });
+        var message = new Message({ message: "some message" })
+        expect(message.message).toEqual("some message")
+      })
 
     it("has an author id", () => {
-        var message = new Message({ authorId: mockUser });
-        expect(message.authorId).toEqual(mockUser);
-    });
+        var message = new Message({ authorId: mockUser })
+        expect(message.authorId).toEqual(mockUser)
+    })
 
     it("has a reaction", () => {
-        var message = new Message({ reactions: { user: mockUser, emoji: "😊" }});
-        expect(message.reactions[0].user).toEqual(mockUser);
-        expect(message.reactions[0].emoji).toEqual("😊");
-    });
+        var message = new Message({ reactions: { user: mockUser, emoji: "😊" }})
+        expect(message.reactions[0].user).toEqual(mockUser)
+        expect(message.reactions[0].emoji).toEqual("😊")
+    })
 
     it("shows a reply to a message", () => {
-        const mockMessage = new Message({ message: "mock message" });
-        var message = new Message({ isReplyTo: mockMessage});
-        expect(message.isReplyTo).toEqual(mockMessage);
-    });
+        const mockMessage = new Message({ message: "mock message" })
+        var message = new Message({ isReplyTo: mockMessage})
+        expect(message.isReplyTo).toEqual(mockMessage)
+    })
 
     it("has the time of post creation", () => {
         const mockDateObject = new Date("2022-04-20T13:33:42.767Z")
@@ -61,5 +63,10 @@ describe("Message model", () => {
         spy.mockRestore()
     
         expect(message.timeStamp).toEqual(new Date("2022-04-20T13:33:42.767Z"))
-      });
-});
+    })
+
+    it("displays the chat room", () => {
+      var message = new Message({ message: "some message", roomName: "Ruby" })
+      expect(message.roomName).toEqual("Ruby")
+    })
+})
