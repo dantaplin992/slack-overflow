40e14458289a2b57ad0b79452c6264864816881a
Jon Clarke <jonclarke1984@googlemail.com>
2022-05-12 11:38:19 +0100

    hope this works

---
+++ client/src/components/utils/emailExists.js
@@ -0,0 +1,19 @@
+export function emailExists(email) {
+  fetch(`http://localhost:5000/users/email/check`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email: email }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('email check result from server: ', data.message)
+      if (data.message == 'userExists') {
+        alert('Email already exists')
+        return true
+      }
+      return false
+    }
+    )
+}
